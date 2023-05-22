6814c4a9ac3bef1ed4e743fb6f153525a4ced73a
Thomas Fenaroli <thomas.s.fenaroli.24@dartmouth.edu>
2023-05-22 17:23:15 -0400

    firebase admin sdk initialization

--- src/router.js
+++ src/router.js
@@ -1,12 +1,9 @@
 import { Router } from 'express';
-// import { CastError } from 'mongoose'; // chatGPT
-// import * as Posts from './controllers/chunk_controller';
-// import signS3 from './services/s3';
 
 const router = Router();
 
-router.get('/', (req, res) => {
-  res.json({ message: 'welcome to our blog api!' });
+router.get('/gpt', (req, res) => {
+  res.json({ response: 'this is a test summary' });
 });
 
 export default router;
