c6787afa51fff4acd698ea0362b2164f26c991db
Stan Gao <34614007+stanl37@users.noreply.github.com>
2023-05-22 14:46:28 -0400

    first pass openai.js

---
+++ src/services/openai.js
@@ -0,0 +1,21 @@
+// functions to interact with openai API
+
+const { Configuration, OpenAIApi } = require('openai');
+
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+const openai = new OpenAIApi(configuration);
+
+/*
+const response = await openai.createCompletion({
+  model: 'text-davinci-003',
+  prompt: 'Say this is a test',
+  temperature: 0,
+  max_tokens: 7,
+});
+*/
+
+const createCompletion;
+// streaming: https://github.com/openai/openai-node/issues/18#issuecomment-1369996933
+
+const createChatCompletion;
+
