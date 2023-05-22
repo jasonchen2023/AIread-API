2a6c8e72ce84837c91b3df31f906098cf8549e25
Thomas Fenaroli <thomas.s.fenaroli.24@dartmouth.edu>
2023-05-22 14:59:39 -0400

    clean-up

--- src/services/openai.js
+++ src/services/openai.js
@@ -1,21 +1,20 @@
-// functions to interact with openai API
+// // functions to interact with openai API
 
-const { Configuration, OpenAIApi } = require('openai');
+// const { Configuration, OpenAIApi } = require('openai');
 
-const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
-const openai = new OpenAIApi(configuration);
+// const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+// const openai = new OpenAIApi(configuration);
 
-/*
-const response = await openai.createCompletion({
-  model: 'text-davinci-003',
-  prompt: 'Say this is a test',
-  temperature: 0,
-  max_tokens: 7,
-});
-*/
+// /*
+// const response = await openai.createCompletion({
+//   model: 'text-davinci-003',
+//   prompt: 'Say this is a test',
+//   temperature: 0,
+//   max_tokens: 7,
+// });
+// */
 
-const createCompletion;
-// streaming: https://github.com/openai/openai-node/issues/18#issuecomment-1369996933
+// const createCompletion;
+// // streaming: https://github.com/openai/openai-node/issues/18#issuecomment-1369996933
 
-const createChatCompletion;
-
+// const createChatCompletion;
