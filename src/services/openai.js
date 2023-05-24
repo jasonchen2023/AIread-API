dee61d598ae96f8016ca7851a95594f04232c7e2
Thomas Fenaroli <thomas.s.fenaroli.24@dartmouth.edu>
2023-05-24 14:57:11 -0400

    refactor

--- src/services/openai.js
+++ src/services/openai.js
@@ -1,13 +1,18 @@
+/* eslint-disable import/prefer-default-export */
 import { Configuration, OpenAIApi } from 'openai';
 
-const configuration = new Configuration({ apiKey: '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' });
-// const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+require('dotenv').config();
+
+const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+
 const openai = new OpenAIApi(configuration);
 
 // PROMPT
 // can adjust this to fine-tune model output
+
 const prompt = 'You are an AI summarization agent. Your goal is to distill information down for readers to accelerate learning and comphrehension. Please summarize the following text to bullets, while preserving as much important information as possible.';
 
+// gets summary for a chunk of text
 const getSummary = async (content) => {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
@@ -19,4 +24,12 @@ const getSummary = async (content) => {
   return summary;
 };
 
-export default getSummary;
+// processes an entire document
+const processText = async (content) => {
+  const chunks = content.split('\n').map((chunk) => { return chunk.trim(); });
+  const summaries = await Promise.all(chunks.map((chunk) => { return getSummary(chunk); }));
+  const tuples = chunks.map((chunk, index) => { return [chunk, summaries[index]]; });
+  return tuples;
+};
+
+export { processText };
