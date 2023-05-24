d06c609ed6f7fce10b6da0c77ffa9bccfc93188a
Stan Gao <34614007+stanl37@users.noreply.github.com>
2023-05-24 00:32:02 -0400

    working (basic version) openai summarization

--- src/services/openai.js
+++ src/services/openai.js
@@ -1,40 +1,22 @@
-// functions to interact with openai API
+import { Configuration, OpenAIApi } from 'openai';
 
-const { Configuration, OpenAIApi } = require('openai');
-
-const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+const configuration = new Configuration({ apiKey: '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' });
+// const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 
+// PROMPT
+// can adjust this to fine-tune model output
 const prompt = 'You are an AI summarization agent. Your goal is to distill information down for readers to accelerate learning and comphrehension. Please summarize the following text to bullets, while preserving as much important information as possible.';
 
-/*
-const response = await openai.createCompletion({
-  model: 'text-davinci-003',
-  prompt: 'Say this is a test'
-});
-*/
-
-export async function getSummary(content, res) {
-  const summary = 'yeet!';
-
-  const chunk = {
-    content,
-    summary,
-  };
-  return chunk;
-}
-
-export async function promptOpenAI(content) {
+const getSummary = async (content) => {
   const response = await openai.createCompletion({
-    model: 'davinci',
-    prompt: `${prompt}\n${content}`,
-    temperature: 0.5,
-    max_tokens: 60,
-    stop: ['\n'],
+    model: 'text-davinci-003',
+    prompt: `${prompt}\n\n${content}`,
+    max_tokens: 1000,
   });
 
-  const summary = response.choices[0].text.trim();
+  const summary = response.data.choices[0].text.trim();
   return summary;
-}
+};
 
-// streaming: https://github.com/openai/openai-node/issues/18#issuecomment-1369996933
+export default getSummary;
