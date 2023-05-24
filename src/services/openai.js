00f8ac4470a3ffee7a6c852d0f4df513375226ca
Stan Gao <34614007+stanl37@users.noreply.github.com>
2023-05-24 14:58:34 -0400

    working on prompt

--- src/services/openai.js
+++ src/services/openai.js
@@ -7,14 +7,28 @@ dotenv.config({ silent: true });
 const configuration = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(configuration);
 
-// PROMPT
+// PROMPT DESIGN
 // can adjust this to fine-tune model output
-const prompt = 'You are an AI summarization agent. Your goal is to distill information down for readers to accelerate learning and comphrehension. Please summarize the following text to bullets, while preserving as much important information as possible.';
+// =============================================================================
+const buildPrompt = (content) => {
+  
+  // replace this with input from content now
+  const age = '21';
 
+  const background = 'You are an AI summarization agent. Your goal is to distill information down for readers to accelerate learning and comphrehension.';
+  const context = `The reader is ${age} `;
+  const instructions = 'Your summaries should preserve as much important information as possible.';
+  const task = 'Please summarize the following text, bullet form.';
+  const prompt = `${background}\n${context} ${instructions}\n${task}`;
+  return prompt;
+};
+
+// SUMMARY LOGIC
+// =============================================================================
 const getSummary = async (content) => {
   const response = await openai.createCompletion({
     model: 'text-davinci-003',
-    prompt: `${prompt}\n\n${content}`,
+    prompt: buildPrompt(content),
     max_tokens: 1000,
   });
 
