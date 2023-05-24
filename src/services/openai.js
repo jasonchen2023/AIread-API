288e33699779dc8147c6fa6f45497e4ea3fb4190
Thomas Fenaroli <thomas.s.fenaroli.24@dartmouth.edu>
2023-05-24 15:17:22 -0400

    chat endpiont

--- src/services/openai.js
+++ src/services/openai.js
@@ -33,4 +33,15 @@ const processText = async (content) => {
   return tuples;
 };
 
-export { processText };
+// process an isolated chat request
+const chat = async (content) => {
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `${content}`,
+    max_tokens: 1000,
+  });
+
+  return response.data.choices[0].text;
+};
+
+export { chat, processText };
